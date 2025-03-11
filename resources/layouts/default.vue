c5395b51cce49af5ac82720fec8a9c704d6cbe91
pINKmUNster <floriansieradzki@gmail.com>
2025-03-11 10:19:15 +0100

    use route

--- resources/layouts/default.vue
+++ resources/layouts/default.vue
@@ -3,24 +3,21 @@ useHead({
     title: 'Refuel'
 })
 </script>
-
 <template>
     <section class="bg-gray-200 p-6">
         <header class="flex justify-between">
             <div class="flex items-center">
-                <h1 class="text-lg font-bold">Refuel</h1>
+                <router-link :href="route('homepage.index')">
+                    <h1 class="text-lg font-bold">Refuel</h1>
+                </router-link>
                 <p class="ml-4 text-sm">Welcome Back</p>
             </div>
-            <Nav />
+            <Nav/>
         </header>
     </section>
     <section class="p-6">
         <div class="mx-auto max-w-3xl">
-            <slot />
+            <slot/>
         </div>
     </section>
 </template>
-
-<style scoped>
-
-</style>

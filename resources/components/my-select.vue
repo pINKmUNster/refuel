ca7acc35e5cef788428e252d789f8b5237e1f72b
pINKmUNster <floriansieradzki@gmail.com>
2025-03-11 14:57:36 +0100

    wip

--- resources/components/my-select.vue
+++ resources/components/my-select.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import {Label} from "reka-ui";
+
 export interface SelectOption {
     value: string | number;
     label: string;
@@ -15,7 +17,7 @@ const value = defineModel();
 
 <template>
     <div>
-        <label :for="name">{{ label }}</label>
+        <Label :for="name">{{ label }}</Label>
         <select :name="name" :id="name" v-model="value" class="w-full border rounded">
             <slot name="option"></slot>
             <option v-for="option in selectOptions" :value="option.value" v-text="option.label"/>
--- src/Modules/Vehicle/Web/views/show.vue
+++ src/Modules/Vehicle/Web/views/show.vue
@@ -6,12 +6,27 @@ defineProps<{
 
 <template layout>
     <div class="vehicle">
-        <h2 class="font-bold uppercase">{{ vehicle.licencePlate }}</h2>
-        <p>{{ vehicle.vin }}</p>
-        <p>{{ vehicle.brand }}</p>
-        <p>{{ vehicle.type }}</p>
-        <p>{{ vehicle.fuelType }}</p>
-        <p>{{ vehicle.owner}}</p>
+        <h2 class="font-bold uppercase text-3xl">{{ vehicle.licence_plate }}</h2>
+        <div class="grid grid-cols-2 gap-4 mt-4">
+            <label class="font-bold">VIN:</label>
+            <p> {{ vehicle.vin }}</p>
+        </div>
+        <div class="grid grid-cols-2 gap-2 mt-4">
+            <label class="font-bold">Brand:</label>
+            <p> {{ vehicle.brand }}</p>
+        </div>
+        <div class="grid grid-cols-2 gap-2 mt-4">
+            <label class="font-bold">Type:</label>
+            <p> {{ vehicle.type }}</p>
+        </div>
+        <div class="grid grid-cols-2 gap-2 mt-4">
+            <label class="font-bold">Fuel type:</label>
+            <p> {{ vehicle.fuelType }}</p>
+        </div>
+        <div class="grid grid-cols-2 gap-2 mt-4">
+            <label class="font-bold">Owner:</label>
+            <p> {{ vehicle.owner }}</p>
+        </div>
     </div>
 </template>
 

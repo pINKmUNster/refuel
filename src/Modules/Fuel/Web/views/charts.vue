62b1886b683f39452470742ae4689a69e82f1b60
pINKmUNster <floriansieradzki@gmail.com>
2025-03-14 10:32:56 +0100

    add charts

--- src/Modules/Fuel/Web/views/charts.vue
+++ src/Modules/Fuel/Web/views/charts.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+import {VisXYContainer , VisLine, VisAxis} from '@unovis/vue'
+import {ref} from 'vue'
 
+type DataRecord = { x: number, y: number }
+const data = ref<DataRecord[]>([
+    {x: 0, y: 0},
+    {x: 1, y: 2},
+    {x: 2, y: 1},
+])
 </script>
 
 <template layout>
+    <VisXYContainer  :data="data">
+        <VisLine :x="d => d.x" :y="d => d.y"/>
+        <VisAxis type="x"/>
+        <VisAxis type="y"/>
+    </VisXYContainer >
 </template>
 
 <style scoped>
